b8395266d30b729b80cbc7f15c5059cd6b7103b7
Michael Cary <39915578+cat-astrophic@users.noreply.github.com>
2024-08-27 14:20:41 -0400

    creates data frame for regression analysis

--- ultrasignup_merger.py
+++ ultrasignup_merger.py
@@ -18,12 +18,11 @@ epa_direc = 'D:/EPA/'
 # Reading in the data
 
 data = pd.read_csv(direc + 'temp_data.csv')
-pm = pd.read_csv(epa_direc + 'epa_aqs_data_pm.csv')
-pm10 = pd.read_csv(epa_direc + 'epa_aqs_data_pm10.csv')
-o3 = pd.read_csv(epa_direc + 'epa_aqs_data_ozone.csv')
-no2 = pd.read_csv(epa_direc + 'epa_aqs_data_no2.csv')
-co = pd.read_csv(epa_direc + 'epa_aqs_data_co.csv')
-pb = pd.read_csv(epa_direc + 'epa_aqs_data_pb.csv')
+pm = pd.read_csv(epa_direc + 'epa_aqs_data_PM.csv')
+pm10 = pd.read_csv(epa_direc + 'epa_aqs_data_PM10.csv')
+o3 = pd.read_csv(epa_direc + 'epa_aqs_data_O3.csv')
+no2 = pd.read_csv(epa_direc + 'epa_aqs_data_NO2.csv')
+co = pd.read_csv(epa_direc + 'epa_aqs_data_CO.csv')
 
 # Creata a FIPS code column for pollution
 
@@ -62,14 +61,12 @@ pm10_fips = [fips_fx(pm10,i) for i in range(len(pm10))]
 o3_fips = [fips_fx(o3,i) for i in range(len(o3))]
 no2_fips = [fips_fx(no2,i) for i in range(len(no2))]
 co_fips = [fips_fx(co,i) for i in range(len(co))]
-pb_fips = [fips_fx(pb,i) for i in range(len(pb))]
 
 pm = pd.concat([pm, pd.Series(pm_fips, name = 'FIPS')], axis = 1)
 pm10 = pd.concat([pm10, pd.Series(pm10_fips, name = 'FIPS')], axis = 1)
 o3 = pd.concat([o3, pd.Series(o3_fips, name = 'FIPS')], axis = 1)
 no2 = pd.concat([no2, pd.Series(no2_fips, name = 'FIPS')], axis = 1)
 co = pd.concat([co, pd.Series(co_fips, name = 'FIPS')], axis = 1)
-pb = pd.concat([pb, pd.Series(pb_fips, name = 'FIPS')], axis = 1)
 
 # Creating county-day pollution level data
 
@@ -78,7 +75,6 @@ good_pm10 = pm10.groupby(['FIPS', 'Date']).mean().reset_index()
 good_o3 = o3.groupby(['FIPS', 'Date']).mean().reset_index()
 good_no2 = no2.groupby(['FIPS', 'Date']).mean().reset_index()
 good_co = co.groupby(['FIPS', 'Date']).mean().reset_index()
-good_pb = pb.groupby(['FIPS', 'Date']).mean().reset_index()
 
 # Mapping cities to counties in data
 
@@ -254,14 +250,12 @@ dd_pm10 = [date_fixer_2(good_pm10.Date[i]) for i in range(len(good_pm10))]
 dd_o3 = [date_fixer_2(good_o3.Date[i]) for i in range(len(good_o3))]
 dd_no2 = [date_fixer_2(good_no2.Date[i]) for i in range(len(good_no2))]
 dd_co = [date_fixer_2(good_co.Date[i]) for i in range(len(good_co))]
-dd_pb = [date_fixer_2(good_pb.Date[i]) for i in range(len(good_pb))]
 
 good_pm = pd.concat([good_pm, pd.Series(dd_pm, name = 'PM_Date')], axis = 1)
 good_pm10 = pd.concat([good_pm10, pd.Series(dd_pm10, name = 'PM10_Date')], axis = 1)
 good_o3 = pd.concat([good_o3, pd.Series(dd_o3, name = 'O3_Date')], axis = 1)
 good_no2 = pd.concat([good_no2, pd.Series(dd_no2, name = 'NO2_Date')], axis = 1)
 good_co = pd.concat([good_co, pd.Series(dd_co, name = 'CO_Date')], axis = 1)
-good_pb = pd.concat([good_pb, pd.Series(dd_pb, name = 'Pb_Date')], axis = 1)
 
 # Cleaning pollution FIPS
 
@@ -270,14 +264,12 @@ pm10_fips = [f if len(f) == 5 else '0' + f for f in good_pm10.FIPS]
 o3_fips = [f if len(f) == 5 else '0' + f for f in good_o3.FIPS]
 no2_fips = [f if len(f) == 5 else '0' + f for f in good_no2.FIPS]
 co_fips = [f if len(f) == 5 else '0' + f for f in good_co.FIPS]
-pb_fips = [f if len(f) == 5 else '0' + f for f in good_pb.FIPS]
 
 good_pm = pd.concat([good_pm, pd.Series(pm_fips, name = 'PM_FIPS')], axis = 1)
 good_pm10 = pd.concat([good_pm10, pd.Series(pm10_fips, name = 'PM10_FIPS')], axis = 1)
 good_o3 = pd.concat([good_o3, pd.Series(o3_fips, name = 'O3_FIPS')], axis = 1)
 good_no2 = pd.concat([good_no2, pd.Series(no2_fips, name = 'NO2_FIPS')], axis = 1)
 good_co = pd.concat([good_co, pd.Series(co_fips, name = 'CO_FIPS')], axis = 1)
-good_pb = pd.concat([good_pb, pd.Series(pb_fips, name = 'Pb_FIPS')], axis = 1)
 
 # Creating the event location pollution data
 
@@ -286,7 +278,6 @@ pm10_data = []
 o3_data = []
 no2_data = []
 co_data = []
-pb_data = []
 
 for i in range(len(data)):
     
@@ -297,14 +288,12 @@ for i in range(len(data)):
     tmp_o3 = good_o3[good_o3.O3_FIPS == data.Event_FIPS[i]]
     tmp_no2 = good_no2[good_no2.NO2_FIPS == data.Event_FIPS[i]]
     tmp_co = good_co[good_co.CO_FIPS == data.Event_FIPS[i]]
-    tmp_pb = good_pb[good_pb.Pb_FIPS == data.Event_FIPS[i]]
     
     tmp_pm = tmp_pm[tmp_pm.PM_Date == data.Race_Date[i]].reset_index(drop = True)
     tmp_pm10 = tmp_pm10[tmp_pm10.PM10_Date == data.Race_Date[i]].reset_index(drop = True)
     tmp_o3 = tmp_o3[tmp_o3.O3_Date == data.Race_Date[i]].reset_index(drop = True)
     tmp_no2 = tmp_no2[tmp_no2.NO2_Date == data.Race_Date[i]].reset_index(drop = True)
     tmp_co = tmp_co[tmp_co.CO_Date == data.Race_Date[i]].reset_index(drop = True)
-    tmp_pb = tmp_pb[tmp_pb.Pb_Date == data.Race_Date[i]].reset_index(drop = True)
     
     if len(tmp_pm) > 0:
         
@@ -345,23 +334,14 @@ for i in range(len(data)):
     else:
         
         co_data.append(None)
-        
-    if len(tmp_pb) > 0:
-        
-        pb_data.append(tmp_pb.Value[0])
-        
-    else:
-        
-        pb_data.append(None)
 
 pm_data = pd.Series(pm_data, name = 'PM')
 pm10_data = pd.Series(pm10_data, name = 'PM10')
 o3_data = pd.Series(o3_data, name = 'O3')
 no2_data = pd.Series(no2_data, name = 'NO2')
 co_data = pd.Series(co_data, name = 'CO')
-pb_data = pd.Series(pb_data, name = 'Pb')
 
-data = pd.concat([data, pm_data, pm10_data, o3_data, no2_data, co_data, pb_data], axis = 1)
+data = pd.concat([data, pm_data, pm10_data, o3_data, no2_data, co_data], axis = 1)
 
 # Creating the runner location pollution data
 
@@ -370,28 +350,24 @@ rnr_pm10_data_30 = []
 rnr_o3_data_30 = []
 rnr_no2_data_30 = []
 rnr_co_data_30 = []
-rnr_pb_data_30 = []
 
 rnr_pm_data_60 = []
 rnr_pm10_data_60 = []
 rnr_o3_data_60 = []
 rnr_no2_data_60 = []
 rnr_co_data_60 = []
-rnr_pb_data_60 = []
 
 rnr_pm_data_90 = []
 rnr_pm10_data_90 = []
 rnr_o3_data_90 = []
 rnr_no2_data_90 = []
 rnr_co_data_90 = []
-rnr_pb_data_90 = []
 
 rnr_pm_data_180 = []
 rnr_pm10_data_180 = []
 rnr_o3_data_180 = []
 rnr_no2_data_180 = []
 rnr_co_data_180 = []
-rnr_pb_data_180 = []
 
 for i in range(len(data)):
     
@@ -402,43 +378,36 @@ for i in range(len(data)):
     o3_tmp = good_o3[good_o3.FIPS == data.Event_FIPS[i]]
     no2_tmp = good_no2[good_no2.FIPS == data.Event_FIPS[i]]
     co_tmp = good_co[good_co.FIPS == data.Event_FIPS[i]]
-    pb_tmp = good_pb[good_pb.FIPS == data.Event_FIPS[i]]
     
     pm_tmp = pm_tmp[pm_tmp.PM_Date < data.Race_Date[i]]
     pm10_tmp = pm10_tmp[pm10_tmp.PM10_Date < data.Race_Date[i]]
     o3_tmp = o3_tmp[o3_tmp.O3_Date < data.Race_Date[i]]
     no2_tmp = no2_tmp[no2_tmp.NO2_Date < data.Race_Date[i]]
-    co_tmp = co_tmp[co_tmp.CO_Date < data.Race_Date[i]]
-    pb_tmp = pb_tmp[pb_tmp.Pb_Date < data.Race_Date[i]]
-    
+    co_tmp = co_tmp[co_tmp.CO_Date < data.Race_Date[i]]    
     
     pm_tmp = pm_tmp[pm_tmp.PM_Date >= data.Race_Date[i] - timedelta(180)]
     pm10_tmp = pm10_tmp[pm10_tmp.PM10_Date >= data.Race_Date[i] - timedelta(180)]
     o3_tmp = o3_tmp[o3_tmp.O3_Date >= data.Race_Date[i] - timedelta(180)]
     no2_tmp = no2_tmp[no2_tmp.NO2_Date >= data.Race_Date[i] - timedelta(180)]
     co_tmp = co_tmp[co_tmp.CO_Date >= data.Race_Date[i] - timedelta(180)]
-    pb_tmp = pb_tmp[pb_tmp.Pb_Date >= data.Race_Date[i] - timedelta(180)]
     
     pm_tmp2 = pm_tmp[pm_tmp.PM_Date >= data.Race_Date[i] - timedelta(90)]
     pm10_tmp2 = pm10_tmp[pm10_tmp.PM10_Date >= data.Race_Date[i] - timedelta(90)]
     o3_tmp2 = o3_tmp[o3_tmp.O3_Date >= data.Race_Date[i] - timedelta(90)]
     no2_tmp2 = no2_tmp[no2_tmp.NO2_Date >= data.Race_Date[i] - timedelta(90)]
     co_tmp2 = co_tmp[co_tmp.CO_Date >= data.Race_Date[i] - timedelta(90)]
-    pb_tmp2 = pb_tmp[pb_tmp.Pb_Date >= data.Race_Date[i] - timedelta(90)]
     
     pm_tmp3 = pm_tmp[pm_tmp.PM_Date >= data.Race_Date[i] - timedelta(60)]
     pm10_tmp3 = pm10_tmp[pm10_tmp.PM10_Date >= data.Race_Date[i] - timedelta(60)]
     o3_tmp3 = o3_tmp[o3_tmp.O3_Date >= data.Race_Date[i] - timedelta(60)]
     no2_tmp3 = no2_tmp[no2_tmp.NO2_Date >= data.Race_Date[i] - timedelta(60)]
     co_tmp3 = co_tmp[co_tmp.CO_Date >= data.Race_Date[i] - timedelta(60)]
-    pb_tmp3 = pb_tmp[pb_tmp.Pb_Date >= data.Race_Date[i] - timedelta(60)]
     
     pm_tmp4 = pm_tmp[pm_tmp.PM_Date >= data.Race_Date[i] - timedelta(30)]
     pm10_tmp4 = pm10_tmp[pm10_tmp.PM10_Date >= data.Race_Date[i] - timedelta(30)]
     o3_tmp4 = o3_tmp[o3_tmp.O3_Date >= data.Race_Date[i] - timedelta(30)]
     no2_tmp4 = no2_tmp[no2_tmp.NO2_Date >= data.Race_Date[i] - timedelta(30)]
     co_tmp4 = co_tmp[co_tmp.CO_Date >= data.Race_Date[i] - timedelta(30)]
-    pb_tmp4 = pb_tmp[pb_tmp.Pb_Date >= data.Race_Date[i] - timedelta(30)]
     
     rnr_pm_data_30.append(np.mean(pm_tmp4.Value))
     rnr_pm_data_60.append(np.mean(pm_tmp3.Value))
@@ -464,44 +433,35 @@ for i in range(len(data)):
     rnr_co_data_60.append(np.mean(co_tmp3.Value))
     rnr_co_data_90.append(np.mean(co_tmp2.Value))
     rnr_co_data_180.append(np.mean(co_tmp.Value))
-        
-    rnr_pb_data_30.append(np.mean(pb_tmp4.Value))
-    rnr_pb_data_60.append(np.mean(pb_tmp3.Value))
-    rnr_pb_data_90.append(np.mean(pb_tmp2.Value))
-    rnr_pb_data_180.append(np.mean(pb_tmp.Value))
 
 rnr_pm_data_30 = pd.Series(rnr_pm_data_30, name = 'PM_Runner_30')
 rnr_pm10_data_30 = pd.Series(rnr_pm10_data_30, name = 'PM10_Runner_30')
 rnr_o3_data_30 = pd.Series(rnr_o3_data_30, name = 'O3_Runner_30')
 rnr_no2_data_30 = pd.Series(rnr_no2_data_30, name = 'NO2_Runner_30')
 rnr_co_data_30 = pd.Series(rnr_co_data_30, name = 'CO_Runner_30')
-rnr_pb_data_30 = pd.Series(rnr_pb_data_30, name = 'Pb_Runner_30')
 
 rnr_pm_data_60 = pd.Series(rnr_pm_data_60, name = 'PM_Runner_60')
 rnr_pm10_data_60 = pd.Series(rnr_pm10_data_60, name = 'PM10_Runner_60')
 rnr_o3_data_60 = pd.Series(rnr_o3_data_60, name = 'O3_Runner_60')
 rnr_no2_data_60 = pd.Series(rnr_no2_data_60, name = 'NO2_Runner_60')
 rnr_co_data_60 = pd.Series(rnr_co_data_60, name = 'CO_Runner_60')
-rnr_pb_data_60 = pd.Series(rnr_pb_data_60, name = 'Pb_Runner_60')
 
 rnr_pm_data_90 = pd.Series(rnr_pm_data_90, name = 'PM_Runner_90')
 rnr_pm10_data_90 = pd.Series(rnr_pm10_data_90, name = 'PM10_Runner_90')
 rnr_o3_data_90 = pd.Series(rnr_o3_data_90, name = 'O3_Runner_90')
 rnr_no2_data_90 = pd.Series(rnr_no2_data_90, name = 'NO2_Runner_90')
 rnr_co_data_90 = pd.Series(rnr_co_data_90, name = 'CO_Runner_90')
-rnr_pb_data_90 = pd.Series(rnr_pb_data_90, name = 'Pb_Runner_90')
 
 rnr_pm_data_180 = pd.Series(rnr_pm_data_180, name = 'PM_Runner_180')
 rnr_pm10_data_180 = pd.Series(rnr_pm10_data_180, name = 'PM10_Runner_180')
 rnr_o3_data_180 = pd.Series(rnr_o3_data_180, name = 'O3_Runner_180')
 rnr_no2_data_180 = pd.Series(rnr_no2_data_180, name = 'NO2_Runner_180')
 rnr_co_data_180 = pd.Series(rnr_co_data_180, name = 'CO_Runner_180')
-rnr_pb_data_180 = pd.Series(rnr_pb_data_180, name = 'Pb_Runner_180')
 
-data = pd.concat([data, rnr_pm_data_30, rnr_pm10_data_30, rnr_o3_data_30, rnr_no2_data_30, rnr_co_data_30, rnr_pb_data_30,
-                  rnr_pm_data_60, rnr_pm10_data_60, rnr_o3_data_60, rnr_no2_data_60, rnr_co_data_60, rnr_pb_data_60,
-                  rnr_pm_data_90, rnr_pm10_data_90, rnr_o3_data_90, rnr_no2_data_90, rnr_co_data_90, rnr_pb_data_90,
-                  rnr_pm_data_180, rnr_pm10_data_180, rnr_o3_data_180, rnr_no2_data_180, rnr_co_data_180, rnr_pb_data_180,], axis = 1)
+data = pd.concat([data, rnr_pm_data_30, rnr_pm10_data_30, rnr_o3_data_30, rnr_no2_data_30, rnr_co_data_30,
+                  rnr_pm_data_60, rnr_pm10_data_60, rnr_o3_data_60, rnr_no2_data_60, rnr_co_data_60, 
+                  rnr_pm_data_90, rnr_pm10_data_90, rnr_o3_data_90, rnr_no2_data_90, rnr_co_data_90,
+                  rnr_pm_data_180, rnr_pm10_data_180, rnr_o3_data_180, rnr_no2_data_180, rnr_co_data_180], axis = 1)
 
 # Reading in a file I made with event types indicating which ones to keep
 
